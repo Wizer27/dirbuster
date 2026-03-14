00ebdca750efa4f362ee49384d75cf59fe49961c
Wizer27 <svd2yar@gmail.com>
2026-03-14 18:08:52 +0300

    first version

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
-	"flag"
+	"strconv"
 )
 
 func main() {
-	file, err := os.Open("dirs.txt")
+	filename := flag.String("f", "", "File with dirs")
+
+	url := flag.String("u", "", "Url to attack")
+
+	flag.Parse()
+
+	file, err := os.Open(*filename)
 
 	if err != nil {
 		log.Fatal(err)
@@ -21,7 +29,19 @@ func main() {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		test_ur := *url + line
+		resp, err := http.Get(test_ur)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if resp.StatusCode != 404 {
+			status_code_str := strconv.Itoa(resp.StatusCode) 
+			fmt.Println("[+] " + test_ur)
+			fmt.Println("[+] Status code " + status_code_str)
+		}
+		defer resp.Body.Close()
+
 	}
 
 	if err := scanner.Err(); err != nil {
