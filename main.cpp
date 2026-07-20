55a21788cd02c3f70c2a195d43bf9ff78cc6d717
Wizer27 <svd2yar@gmail.com>
2026-07-20 15:58:22 +0300

    .

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <CLI/CLI.hpp>
 
 using namespace std;
 
 
-int main(){
+int main(int argc,char** argv){
+    CLI::App app{"Dirbuster"};
+    std::string wordlist;
+    std::string url;
+
+
+    app.add_option("-u,--url",url,"Target url") -> required();
+    app.add_option("-f,--file",wordlist,"Wordlist") -> required();
+
+    CLI11_PARSE(app, argc, argv);
+
+    std::cout << "URL: " << url << '\n';
+    std::cout << "Wordlist: " << wordlist << '\n';
     return 0;
 }
