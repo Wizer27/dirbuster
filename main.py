d8d894f7b9071890370b5499a5f160b48deeb8f1
Wizer27 <svd2yar@gmail.com>
2026-08-09 14:18:52 +0300

    ffuf style coded the better version

--- main.py
+++ main.py
@@ -11,37 +11,59 @@ parser.add_argument("-c","--codes",type = list,help = "allowed codes")
 args = parser.parse_args()
 
     
+def old_version():
+    if not args.file or not args.url:
+        raise KeyError("Wrong argument")
+    else:
+        with open(args.file,"r") as file:
+            data = file.read()
+        for i in data.split('\n'):
+            try:
+                resp = requests.get(args.url + i)
+                if args.codes:
+                    if resp.status_code in args.codes:
+                        print(f"[+] {args.url + i}")
+                        print(f"[+] Status_code: {resp.status_code}")
+                        if args.json:
+                            print(f"JSON : {resp.json()}")
+                else:
+                    if resp.status_code not in [404,401]:
+                        print(f"[+] {args.url + i}")
+                        print(f"[+] Status_code: {resp.status_code}")
+                        if args.json:
+                            print(f"JSON : {resp.json()}")
 
-if not args.file or not args.url:
-    raise KeyError("Wrong argument")
-else:
-    with open(args.file,"r") as file:
-        data = file.read()
-    for i in data.split('\n'):
-        try:
-            resp = requests.get(args.url + i)
-            if args.codes:
-                if resp.status_code in args.codes:
-                    print(f"[+] {args.url + i}")
-                    print(f"[+] Status_code: {resp.status_code}")
-                    if args.json:
-                        print(f"JSON : {resp.json()}")
-            else:
-                 if resp.status_code not in [404,401]:
-                    print(f"[+] {args.url + i}")
-                    print(f"[+] Status_code: {resp.status_code}")
-                    if args.json:
-                        print(f"JSON : {resp.json()}")
-
-        except Exception as e:
-            print(f"Error : {e}")
+            except Exception as e:
+                print(f"Error : {e}")
 
 
 def ffuf_style():
     if not args.file or not args.url:
         raise KeyError("Wrong argument")
-    
-    with open(args.file,"r") as file:
-        data = file.read()
-    
-    
+    else:
+        with open(args.file,"r") as file:
+            data = file.read()
+        
+        for dir in data.split('\n'):
+            try:
+                url = args.url.replace("FUZZ",dir)
+                resp = requests.get(url)
+                if args.codes:
+                    if resp.status_code in args.codes:
+                        print(f"[+] {url}")
+                        print(f"[+] Status_code: {resp.status_code}")
+                        if args.json:
+                            print(f"JSON : {resp.json()}")
+                else:
+                    if resp.status_code not in [404,401]:
+                        print(f"[+] {url}")
+                        print(f"[+] Status_code: {resp.status_code}")
+                        if args.json:
+                            print(f"JSON : {resp.json()}")
+
+            except Exception as e:
+                print(f"[+] ERROR : {e}")
+
+if __name__ == "__main__":
+    ffuf_style()
+
