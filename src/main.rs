57b69ae2853435c93ddda510cefda9a4214b265c
Wizer27 <svd2yar@gmail.com>
2026-09-10 00:16:56 +0300

    rust coding

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::io;
+use std::io::{BufReader,BufRead};
+use std::fs::File;
 
 
-
-async fn send_request() -> Result<(),reqwest::Error>{
-    let response = reqwest::get("https://httpbin.org/get")
+async fn send_request(url:&str) -> Result<(),reqwest::Error>{
+    let response = reqwest::get(url)
         .await?;
 
     let text = response.text().await?;
@@ -17,6 +17,14 @@ async fn send_request() -> Result<(),reqwest::Error>{
 
 #[tokio::main]
 async fn main(){
-    let response = send_request().await;
-    println!("{:?}",response);
+    let url = "https://google.com/";
+    let file = File::open("dirs.txt").expect("Cannot open file");
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line = line.expect("Cannot read line");
+        let new_url = format!("{}{}", url, line);
+        let response = send_request(&new_url).await;
+        println!("[+]Respone: {:?}",response);
+
+    }
 }
